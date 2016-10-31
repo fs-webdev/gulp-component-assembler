8563be9c16b8253dee056aa2aef802c36422e4d9
Emily Goddard <emilysgoddard@gmail.com>
2016-10-31 15:39:13 -0600

    Add code coverage with Istanbul

--- src/locales.js
+++ src/locales.js
@@ -157,12 +157,12 @@ var flattenObject = function(ob) {
   var toReturn = {};
 
   for (var i in ob) {
-    if (!ob.hasOwnProperty(i)) continue;
+    if (!ob.hasOwnProperty(i)) {continue;}
 
       if ((typeof ob[i]) === 'object') {
         var flatObject = flattenObject(ob[i]);
         for (var x in flatObject) {
-          if (!flatObject.hasOwnProperty(x)) continue;
+          if (!flatObject.hasOwnProperty(x)) {continue;}
 
           toReturn[i + '.' + x] = flatObject[x];
         }
