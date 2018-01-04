4d13bf5021516f04a4b39055f2a287078e2bd176
Joey Cozza <joeycozza@gmail.com>
2018-01-04 15:01:03 -0700

    updating hasChanged function to work with gulp-changed new api

--- src/hasChanged.js
+++ src/hasChanged.js
@@ -2,40 +2,41 @@ var fs = require('fs');
 var path = require('path');
 var PluginError = require('plugin-error');
 
-function compareLastModifiedTime(stream, cb, basePath, sourceFile, targetPath) {
+function compareLastModifiedTime(stream, basePath, sourceFile, targetPath) {
   if (sourceFile.isNull()) {
-    cb(null, sourceFile);
-    return;
+    return new Promise().resolve(sourceFile);
   }
 
-  fs.stat(targetPath, function(err, targetStat) {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        // try reading the file using the oldDest path
-        var target = path.join(basePath, path.basename(basePath)+'.js');
+  return new Promise(function (resolve, reject) {
+    fs.stat(targetPath, function(err, targetStat) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // try reading the file using the oldDest path
+          var target = path.join(basePath, path.basename(basePath)+'.js');
 
-        if (target !== targetPath) {
-          return compareLastModifiedTime(stream, cb, basePath, sourceFile, target);
+          if (target !== targetPath) {
+            return compareLastModifiedTime(stream, basePath, sourceFile, target);
+          }
+          else {
+            stream.push(sourceFile);
+          }
         }
         else {
+          stream.emit('error', new PluginError('gulp-changed', err, {
+            fileName: sourceFile.path
+          }));
+
           stream.push(sourceFile);
         }
       }
-      else {
-        stream.emit('error', new PluginError('gulp-changed', err, {
-          fileName: sourceFile.path
-        }));
 
+      // file changed
+      else if (sourceFile.stat.mtime > targetStat.mtime) {
         stream.push(sourceFile);
       }
-    }
 
-    // file changed
-    else if (sourceFile.stat.mtime > targetStat.mtime) {
-      stream.push(sourceFile);
-    }
-
-    cb();
+      resolve();
+    });
   });
 }
 
@@ -44,9 +45,9 @@ function compareLastModifiedTime(stream, cb, basePath, sourceFile, targetPath) {
  * to it's output file.
  * @see https://github.com/sindresorhus/gulp-changed#haschanged
  */
-module.exports = function (stream, cb, sourceFile, targetPath) {
+module.exports = function (stream, sourceFile, targetPath) {
   var basePath = path.dirname(targetPath);
   targetPath = path.join(path.dirname(basePath), path.basename(basePath)+'.js');
 
-  compareLastModifiedTime(stream, cb, basePath, sourceFile, targetPath);
+  return compareLastModifiedTime(stream, basePath, sourceFile, targetPath);
 };
